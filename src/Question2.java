bc4b8d7d1d31100ad46d395eba6ba211eb5bb48a
이형민 <mini100142@gmail.com>
2024-10-23 15:29:23 +0900

    20241023

---
+++ src/Question2.java
@@ -0,0 +1,6 @@
+public class Question2 {
+    public static void main(String[] args) {
+        int[] numbers = new int[10];
+        System.out.println("배열의 길이: " + numbers.length);
+    }
+}
---
+++ src/Question7.java
@@ -0,0 +1,11 @@
+public class Question7 {
+    public static void main(String[] args) {
+        int[] numbers = {5, 10, 15, 20, 25};
+
+        System.out.println("역순 출력:");
+        for (int i = numbers.length - 1; i >= 0; i--) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
+
